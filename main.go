fabcc752ed89e7573cc17ead01f75b784d077bba
Carlos Martin <s_martincj@globalhitss.com>
2022-09-20 12:58:39 -0500

    controlador, rutas

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-mongo-api/configs"
+	"gin-mongo-api/routes"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"data": "Hello from Gin-gonic & mongoDB",
+	/*
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"data": "Hello from Gin-gonic & mongoDB",
+			})
 		})
-	})
+	*/
+
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(router) //add this
 
 	router.Run(":9090")
 }
